f229da847ab700028b570177cf2f12b82f4794bd
조선근 <@DESKTOP-CEQQROI>
2018-11-30 21:11:16 +0900

    Mybtis 설정

--- src/main/java/github/sunkeun/webboard/dao/RealPostDao.java
+++ src/main/java/github/sunkeun/webboard/dao/RealPostDao.java
@@ -5,11 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 import github.sunkeun.webboard.dto.Post;
@@ -30,30 +34,35 @@ public class RealPostDao implements IPostDao{
 	*/
 	@Inject DataSource ds;
 	
+	// @Inject SqlSessionFactory fac;
+	
+	@Inject SqlSession session;
+	
+	
+	
 	@Override
 	public List<Post> findAll() {
+		List<Post> allPosts = session.selectList("PostMapper.findAll");
+		return allPosts;
+	}
+	/*
+	public List<Post> findAll() {
 		
+		SqlSession session = fac.openSession();
 		try {
-			Connection con = ds.getConnection();			
-			PreparedStatement stmt = con.prepareStatement("select * from posts");
-			ResultSet rs = stmt.executeQuery();
-			List<Post> all = new ArrayList<>();
-			while ( rs.next()) {
-				Integer seq = rs.getInt("seq");
-				String title = rs.getString("title");
-				String content = rs.getString("content");
-				all.add(new Post(seq, title, content));
-			}
-			con.close();
-			return all;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
+			List<Post> allPosts = session.selectList("PostMapper.findAll");
+			return allPosts;			
+		} finally {
+			session.close(); // conn.close();
 		}
 	}
+	*/
 
 	@Override
 	public Post findPostBySeq(Integer no) {
-		try {
+		Post post = session.selectOne("PostMapper.findPostBySeq",no);
+		return post;
+	/*	try {
 			Connection con = ds.getConnection();			
 			PreparedStatement stmt = con.prepareStatement("select * from posts where seq = ? ");
 			stmt.setInt(1, no);
@@ -70,7 +79,7 @@ public class RealPostDao implements IPostDao{
 			return all;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}
+		}*/
 		
 	}
 
@@ -101,40 +110,51 @@ public class RealPostDao implements IPostDao{
 	public void deletetPost(int seq) {
 		// TODO Auto-generated method stub
 		
-		try {
+		session.delete("PostMapper.deletetPost",seq);
+		/*try {
 			Connection con = ds.getConnection();
 			PreparedStatement stmt = con.prepareStatement("delete from posts where seq=?");
 			stmt.setInt(1, seq);
 			int rs = stmt.executeUpdate();
-			/*
+			
 			if(rs!=1){
 				throw new RuntimeException("insert fail!!!");
 			}
-			*/
+			
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}
+		}*/
 		
 	}
 
 	@Override  
 	public void updatePost(int seq, String title, String content) {
+		Map<String, Object> param = new HashMap<>();
+		param.put("t", title);
+		param.put("content", content);
+		param.put("pk", seq);
+		
+		int rupdate = session.update("PostMapper.updatePost", param);
+		if ( rupdate != 1 ) {
+			System.out.println("안 먹었음!!!!" + seq);
+		}
+		
 		// TODO Auto-generated method stub
-		try {
+/*		try {
 			Connection con = ds.getConnection();
 			PreparedStatement stmt = con.prepareStatement("update posts set title =? , content=? where seq =?");
 			stmt.setString(1, title);
 			stmt.setString(2, content);
 			stmt.setInt(3, seq);
 			int rs = stmt.executeUpdate();
-			/*
+			
 			if(rs!=1){
 				throw new RuntimeException("insert fail!!!");
 			}
-			*/
+			
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}
+		}*/
 		
 	}
 
